4ea052f5ac522881c5f9d20e0b0fc71dbadbe0d7
Shobhit Yadav <shobhit999000@gmail.com>
2021-03-05 17:08:02 +0530

    Speech recognition done

--- assistant.py
+++ assistant.py
@@ -25,14 +25,21 @@ def wishme():
 
 def takeCommand():
     r=sr.Recognizer()
-    with sr.Microphone as source:
+    with sr.Microphone() as source:
         print('Listening....')
         r.pause_threshold=1
         audio=r.listen(source)
     try:
         print('Recognizing...')
         query=r.recognize_google(audio,language='en-in')
-        print('User said:',query)
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)
+        print("Say that again please...")
+        return "None"
+    return query
 
 if __name__=="__main__":
     wishme()
+    takeCommand()
