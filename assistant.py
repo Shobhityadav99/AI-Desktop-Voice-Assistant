63c57b88d23e389838de29f8801b149c0ea07836
Shobhit Yadav <shobhit999000@gmail.com>
2021-03-05 16:29:51 +0530

    Wish me func added

--- assistant.py
+++ assistant.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import datetime
 
 engine = pyttsx3.init()
 
@@ -11,5 +12,15 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+def wishme():
+    hour=int(datetime.datetime.now().hour)
+    if hour>0 and hour<=12:
+        speak('Good Morning!')
+    elif hour>12 and hour<=18:
+        speak('Good Evening!')
+    else:
+        speak('Good Night!')
+    speak("Hello i am chintu! How may i help you?")
+
 if __name__=="__main__":
-    speak("Shobhit!")
+    wishme()
