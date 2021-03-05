577e0c9f344424c749b46fc479a304c805f1aa20
Shobhit Yadav <shobhit999000@gmail.com>
2021-03-05 16:16:13 +0530

    Speaking enabled

--- assistant.py
+++ assistant.py
@@ -3,6 +3,13 @@ import pyttsx3
 engine = pyttsx3.init()
 
 voices= engine.getProperty('voices') #getting details of current voice
-print(voices)
+# print(voices)
 
 engine.setProperty('voice', voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+if __name__=="__main__":
+    speak("Shobhit!")
